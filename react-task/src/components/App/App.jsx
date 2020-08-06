affb22aa6cfa09ff9542f96a6bd40831ca37ab89
Mikita Kandratsyeu <nickondr.production@gmail.com>
2020-08-06 11:31:20 +0300

    fix(#0): add additional index.js for each components

--- react-task/src/components/App/App.jsx
+++ react-task/src/components/App/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import styles from './App.module.scss';
 
-import { Footer, Header, Main } from './components';
+import { Footer } from '../Footer';
+import { Header } from '../Header';
+import { Main } from '../Main';
 
 function App({ userData }) {
   return (
