cd61b3849405f18fcb9a775ca909165624794a98
Mikita Kandratsyeu <nickondr.production@gmail.com>
2020-08-15 18:25:48 +0300

    add data in mockedResponse.js

---
+++ react-task/src/api/mockedResponse.js
@@ -0,0 +1,56 @@
+export const mockedResponse = [
+  {
+    id: 1,
+    price: 3099.00,
+    currency: 'BYN',
+    title: 'Ноутбук Apple MacBook Air 13" 2020 MWTJ2',
+    description: '13.3" 2560 x 1600 IPS, Intel Core i3 1000G4 1100 МГц, 8 ГБ, SSD 256 ГБ, '
+      + 'граф. адаптер: встроенный, Mac OS, цвет крышки серый',
+    image: 'https://content2.onliner.by/catalog/device/header/7ebf57383595320cccd242bd3e27f5f7.jpeg',
+  },
+  {
+    id: 2,
+    price: 6354.00,
+    currency: 'BYN',
+    title: 'Ноутбук Apple MacBook Pro 16" 2019 MVVJ2',
+    description: '16.0" 3072x1920 IPS, Intel Core i7 9750H 2600 МГц, 16 ГБ, SSD 512 ГБ,'
+      + ' граф. адаптер: AMD Radeon Pro 5300M 4ГБ, Mac OS, цвет крышки серый',
+    image: 'https://content2.onliner.by/catalog/device/header/db499f52fa0d4b9c31fd30231359b4c1.jpeg',
+  },
+  {
+    id: 3,
+    price: 3999.00,
+    currency: 'BYN',
+    title: 'Ноутбук Apple MacBook Pro 13" Touch Bar 2020 MXK32',
+    description: '13.3" 2560 x 1600 IPS, Intel Core i5 8257U 1400 МГц, 8 ГБ, SSD 256 ГБ, '
+      + 'граф. адаптер: встроенный, Mac OS, цвет крышки серый',
+    image: 'https://content2.onliner.by/catalog/device/header/ba478b0ab23322af9818e16ac15d5456.jpeg',
+  },
+  {
+    id: 4,
+    price: 9499.00,
+    currency: 'BYN',
+    title: 'Ноутбук Apple MacBook Pro 16" 2019 Z0XZ001FH',
+    description: '16.0" 3072x1920 IPS, Intel Core i9 9880H 2300 МГц, 32 ГБ, SSD 512 ГБ,'
+      + ' граф. адаптер: AMD Radeon Pro 5300M 4ГБ, Mac OS, цвет крышки серый',
+    image: 'https://content2.onliner.by/catalog/device/header/db499f52fa0d4b9c31fd30231359b4c1.jpeg',
+  },
+  {
+    id: 5,
+    price: 3246.00,
+    currency: 'BYN',
+    title: 'Ноутбук Apple MacBook Pro 13" Touch Bar 2019 MUHN2',
+    description: '13.3" 2560 x 1600 IPS, Intel Core i5 8257U 1400 МГц, 8 ГБ, SSD 128 ГБ, '
+      + 'граф. адаптер: встроенный, Mac OS, цвет крышки серый',
+    image: 'https://content2.onliner.by/catalog/device/header/d2af9bc8e9d4c70baaea315a09d0e904.jpeg',
+  },
+  {
+    id: 6,
+    price: 7490.00,
+    currency: 'BYN',
+    title: 'Ноутбук Apple MacBook Pro 16" 2019 MVVK2',
+    description: '16.0" 3072x1920 IPS, Intel Core i9 9880H 2300 МГц, 16 ГБ, SSD 1024 ГБ, '
+      + 'граф. адаптер: AMD Radeon Pro 5500M 4ГБ, Mac OS, цвет крышки серый',
+    image: 'https://content2.onliner.by/catalog/device/header/db499f52fa0d4b9c31fd30231359b4c1.jpeg',
+  },
+];
